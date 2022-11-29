ad937f1829cb59537dca8cdd4a08bed597662d46
Ronald Figueroa <ronaldra09gtech@gmail.com>
2022-11-29 20:36:17 +0800

    email to username login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,7 +18,7 @@
 
             <!-- username -->
             <div class="mt-4">
-                <x-input-label for="name" :value="__('Username')" />
+                <x-input-label for="username" :value="__('Username')" />
                 <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus />
                 <x-input-error :messages="$errors->get('username')" class="mt-2" />
             </div>
