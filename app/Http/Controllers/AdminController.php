2911e195a7c723749b1b4402c96f9a7e59f2e6c5
Ronald Figueroa <ronaldra09gtech@gmail.com>
2022-12-01 19:40:57 +0800

    password change View Model and Controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class AdminController extends Controller
@@ -61,5 +62,35 @@ class AdminController extends Controller
         );
 
         return redirect()->route('admin.profile')->with($notification_update_profile);
+    } // End Method
+
+    public function ChangePassword()
+    {
+        return view('admin.admin_change_password');
+    } // End Method
+
+    public function UpdatePassword(Request $request)
+    {
+        $validateData = $request->validate([
+            'oldpassword' => 'required',
+            'newpassword' => 'required',
+            'confirm_password' => 'required|same:newpassword',
+        ]);
+
+        $hashedPassword = Auth::user()->password;
+
+        if (Hash::check($request->oldpassword, $hashedPassword)) {
+            $users = User::find(Auth::id());
+            $users->password = bcrypt($request->newpassword);
+            $users->save();
+
+            session()->flash('message','Password Updated Successfully');
+            return redirect()->back();
+        }
+
+        else{
+            session()->flash('message','Old Password is not match');
+            return redirect()->back();
+        }
     } // End Method
 }
