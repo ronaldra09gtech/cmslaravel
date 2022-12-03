502626278d951c500f81088d2727e0c022c43197
Ronald Figueroa <ronaldra09gtech@gmail.com>
2022-12-04 01:13:51 +0800

    about page setup

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Home\HomeSliderController;
+use App\Http\Controllers\Home\AboutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,9 +33,14 @@ Route::controller(AdminController::class)->group(function () {
 });
 
 //Home Slide All Route
-Route::controller(HomeSliderController::class)->group(function(){
-    Route::get('/home/slide','HomeSlider')->name('home.slide');
-    Route::post('/update/slider','UpdateSlider')->name('update.slider');
+Route::controller(HomeSliderController::class)->group(function () {
+    Route::get('/home/slide', 'HomeSlider')->name('home.slide');
+    Route::post('/update/slider', 'UpdateSlider')->name('update.slider');
+});
+
+//About Page All Route
+Route::controller(AboutController::class)->group(function () {
+    Route::get('/about/page', 'AboutPage')->name('about.page');
 });
 
 Route::get('/dashboard', function () {
