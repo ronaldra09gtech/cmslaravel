f8083198a1ce5b4e39fd4ead9fa23eb89ac91a1c
Ronald Figueroa <ronaldra09gtech@gmail.com>
2022-12-30 21:11:46 +0800

    refreshcode

--- config/app.php
+++ config/app.php
@@ -186,8 +186,6 @@ return [
          * Package Service Providers...
          */
 
-        Intervention\Image\ImageServiceProvider::class,
-
         /*
          * Application Service Providers...
          */
@@ -211,7 +209,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        'Image' => Intervention\Image\Facades\Image::class,
+        // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
 ];
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('username')->unique();
-            $table->varchar('profile_image');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
